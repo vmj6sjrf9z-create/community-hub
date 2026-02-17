557d358122305c87913ed2ff86f95199ad0fbb15
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-17 18:55:30 +0200

    Update Task Card.jsx

--- src/components/TaskCard/Task Card.jsx
+++ src/components/TaskCard/Task Card.jsx
@@ -1,19 +1,22 @@
-import React from "react";
 import "./TaskCard.css";
 
-const TaskCard = ({ title, description }) => {
+function TaskCard({ title, due, color }) {
   return (
     <div className="task-card">
-      <div className="card-icon green">
-        <i className="fas fa-list"></i>
+      
+      <div className={`task-icon ${color}`}>
+        <i className="fas fa-tasks"></i>
       </div>
-      <div className="card-text">
+
+      <div className="task-text">
         <h3>{title}</h3>
-        <p>{description}</p>
+        <p>Due: {due}</p>
       </div>
+
       <i className="fas fa-chevron-right arrow"></i>
+
     </div>
   );
-};
+}
 
 export default TaskCard;
