09f81376010c8ea8b45b110335339c64ff5e521c
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-17 13:39:59 +0200

    Update Greetings.jsx

--- src/components/home/Greetings.jsx
+++ src/components/home/Greetings.jsx
@@ -1,1 +1,14 @@
 //greeting card for app
+
+import React from "react";
+
+const Greeting = () => {
+  return (
+    <section className="greeting">
+      <h1>Good afternoon, Artwell 👋</h1>
+      <p>Here's what's happening today</p>
+    </section>
+  );
+};
+
+export default Greeting;
