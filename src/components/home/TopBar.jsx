70dd50f0748f6678206ae3bce4c2253eb5428db9
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-17 13:36:29 +0200

    Update TopBar.jsx

--- src/components/home/TopBar.jsx
+++ src/components/home/TopBar.jsx
@@ -1,1 +1,16 @@
 //ttop bar for app
+
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <header className="topbar">
+      <img src="https://i.pravatar.cc/100" alt="Avatar" className="avatar" />
+      <div className="bell">
+        <i className="fa-regular fa-bell"></i>
+      </div>
+    </header>
+  );
+};
+
+export default TopBar;
