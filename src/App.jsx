9699339ecad390e079d1e5cc4e6c007bbc480ef3
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-17 06:38:40 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Auth from "./pages/Auth.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
 import Home from "./pages/Home.jsx";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/auth" element={<Auth />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
       <Route path="/" element={<Home />} />
-      <Route path="*" element={<Navigate to="/auth" />} />
+      <Route path="*" element={<Navigate to="/signup" />} />
     </Routes>
   );
 }
