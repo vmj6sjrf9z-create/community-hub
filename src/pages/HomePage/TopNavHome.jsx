67c203665d73d49ee0e71d40cc68768c2b67b243
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-18 14:50:58 +0200

    Update TopNavHome.jsx

--- src/pages/HomePage/TopNavHome.jsx
+++ src/pages/HomePage/TopNavHome.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./TopNavHome.css";
+import { useUser } from "../../context/UserContext.jsx";
 
 const TopNavHome = () => {
+  const { user } = useUser();
+
+  const avatarUrl = user?.user_metadata?.avatar_url || "https://i.pravatar.cc/100";
+
   return (
     <header className="topbar">
-      <img src="https://i.pravatar.cc/100" alt="Avatar" className="avatar" />
+      <img src={avatarUrl} alt="Avatar" className="avatar" />
       <div className="bell">
         <i className="fa-regular fa-bell"></i>
       </div>
