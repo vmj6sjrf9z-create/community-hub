e608cda5f9e3f551478d36350c6ef6df40e6300e
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-21 20:11:55 +0200

    Update HomeRender.jsx

--- src/pages/HomePage/HomeRender.jsx
+++ src/pages/HomePage/HomeRender.jsx
@@ -2,17 +2,9 @@ import TopNavHome from "./TopNavHome";
 import HomeContents from "./HomeContents";
 import BottomNav from "../../components/BottomNav/BottomNav";
 import { useUser } from "../../context/UserContext.jsx";
-import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
 
 function HomeRender() {
   const { user } = useUser();
-  const navigate = useNavigate();
-
-  // Redirect if not logged in
-  useEffect(() => {
-    if (!user) navigate("/signup");
-  }, [user, navigate]);
 
   return (
     <div className="phone">
