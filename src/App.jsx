b98289b7f37da20b362796c6c16b2cfadf8f2d80
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-21 03:20:14 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,29 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 
-// Pages
 import Login from "./pages/Login.jsx";
 import Signup from "./pages/Signup.jsx";
 import HomeRender from "./pages/homePage/homeRender.jsx";
-import TasksRender from "./pages/tasksPage/tasksRender.jsx";
+import TasksRender from "./pages/tasksPage/TasksRender.jsx";
+import TaskInfo from "./pages/tasksPage/TaskInfo.jsx";
 import TeamsRender from "./pages/teamsPage/teamsRender.jsx";
 import SettingsRender from "./pages/settingsPage/settingsRender.jsx";
 
 export default function App() {
   return (
     <Routes>
-      {/* Default redirect */}
       <Route path="/" element={<Navigate to="/signup" />} />
 
-      {/* Auth */}
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
 
-      {/* Main */}
       <Route path="/home" element={<HomeRender />} />
+
       <Route path="/tasks" element={<TasksRender />} />
+      <Route path="/tasks/:id" element={<TaskInfo />} />
+
       <Route path="/teams" element={<TeamsRender />} />
       <Route path="/settings" element={<SettingsRender />} />
 
-      {/* Catch-all */}
       <Route path="*" element={<Navigate to="/signup" />} />
     </Routes>
   );
