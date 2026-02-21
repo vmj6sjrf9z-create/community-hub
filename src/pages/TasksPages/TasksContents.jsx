537f8c9079ba3e2231dd502da303cabb76fa0d49
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-21 03:18:44 +0200

    Update TasksContents.jsx

--- src/pages/TasksPages/TasksContents.jsx
+++ src/pages/TasksPages/TasksContents.jsx
@@ -4,27 +4,19 @@ import TaskCard from "../../components/TaskCard/TaskCard";
 function TasksContents({ tasks, onTaskClick }) {
   return (
     <div className="tasks-page">
-
-      <section className="tasks-greeting">
-        <p>Here are your current tasks</p>
-      </section>
-
-      <section className="tasks-cards">
-        {tasks.length === 0 ? (
-          <p style={{ color: "#9aa4b2", textAlign: "center" }}>No tasks yet</p>
-        ) : (
-          tasks.map((task, index) => (
-            <TaskCard
-              key={index}
-              title={task.title}
-              createdAt={task.createdAt}
-              due={task.due}
-              color={task.color}
-              onClick={() => onTaskClick(task)}
-            />
-          ))
-        )}
-      </section>
+      {tasks.length === 0 ? (
+        <p style={{ color: "#9aa4b2", textAlign: "center" }}>
+          No tasks yet
+        </p>
+      ) : (
+        tasks.map((task) => (
+          <TaskCard
+            key={task.id}
+            {...task}
+            onClick={() => onTaskClick(task)}
+          />
+        ))
+      )}
     </div>
   );
 }
