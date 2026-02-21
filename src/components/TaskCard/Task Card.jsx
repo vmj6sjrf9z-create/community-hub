add054a3b660373125848cbaae03f3dc8130bc97
artwell-dev <vmj6sjrf9z@privaterelay.appleid.com>
2026-02-21 03:19:47 +0200

    Update Task Card.jsx

--- src/components/TaskCard/Task Card.jsx
+++ src/components/TaskCard/Task Card.jsx
@@ -3,19 +3,15 @@ import "./TaskCard.css";
 function TaskCard({ title, due, createdAt, color, onClick }) {
   return (
     <div className="task-card" onClick={onClick}>
-
       <div className={`task-icon ${color}`}>
         <i className="fas fa-tasks"></i>
       </div>
 
       <div className="task-text">
         <h3>{title}</h3>
-        <p className="created">Created: {createdAt}</p>
-        <p className="due">Due: {due}</p>
+        <p>Created: {createdAt}</p>
+        <p>Due: {due}</p>
       </div>
-
-      <i className="fas fa-chevron-right arrow"></i>
-
     </div>
   );
 }
